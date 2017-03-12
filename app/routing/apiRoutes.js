04a5e3a50c639bd3ece589ed04469b696a34a0ae
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2017-03-12 18:52:18 -0400

    cleaned some code up, trying to figure out why newUser being a match sometimes, can't figure it out

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ var friendsData = require("../data/friends");
 
 
 module.exports = function(app) {
-//had this set to friends before but changed it to survey to try and get take survey button to work. may actually need to be friends route
+
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
